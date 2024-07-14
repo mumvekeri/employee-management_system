3ad9e4d3a594fd623dfe818ee0b698480df5521f
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-07-15 00:42:55 +0200

    updates

--- main/myApp/models.py
+++ main/myApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    initials = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField()
+    cell_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    job_title = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    date_joined = models.DateField()
+    picture = models.ImageField(upload_to='employee_pics/')
+    cv = models.FileField(upload_to='employee_cvs/')
+
